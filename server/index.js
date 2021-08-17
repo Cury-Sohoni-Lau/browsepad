c061da59ee85ac472e28b1e6d59f7ad8af607393
mimzivvimzi <mdeniselau@gmail.com>
2021-08-17 22:57:23 +0900

    Add post, delete, and put endpoints

--- server/index.js
+++ server/index.js
@@ -17,6 +17,26 @@ app.get("/notes", async (req, res) => {
   res.send(result);
 });
 
+app.post("/notes", async (req, res) => {
+  const content = req.body;
+  const note = await knex("notes").insert(content);
+  res.send("Yay");
+})
+
+app.delete("/notes/:noteID", async (req, res) => {
+  const noteID = req.params.noteID;
+  await knex("notes").del().where({ id: noteID })
+  res.send("Deleted!")
+})
+
+app.put("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const changedContent = req.body;
+  const note = await knex("notes").where({id: id}).update({title: changedContent.title, content: changedContent.content});
+  res.send("Yay");
+
+})
+
 const startServer = async () => {
   console.log("Running migrations");
   await knex.migrate.latest();
