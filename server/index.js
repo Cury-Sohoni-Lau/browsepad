47451647b4e4c227a9e8dc3023f1ecfe590745e5
flpcury <26260549+flpcury@users.noreply.github.com>
2021-08-18 02:32:47 +0900

    Refactor endpoints

--- server/index.js
+++ server/index.js
@@ -12,30 +12,29 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "../build")));
 
-app.get("/notes", async (req, res) => {
+app.get("/api/notes", async (req, res) => {
   const result = await knex("notes").select();
   res.send(result);
 });
 
-app.post("/notes", async (req, res) => {
+app.post("/api/notes", async (req, res) => {
   const content = req.body;
-  const note = await knex("notes").insert(content);
-  res.send("Yay");
-})
+  await knex("notes").insert(content);
+  res.sendStatus(201);
+});
 
-app.delete("/notes/:noteID", async (req, res) => {
+app.delete("/api/notes/:noteID", async (req, res) => {
   const noteID = req.params.noteID;
-  await knex("notes").del().where({ id: noteID })
-  res.send("Deleted!")
-})
+  await knex("notes").del().where({ id: noteID });
+  res.sendStatus(204);
+});
 
-app.put("/notes/:id", async (req, res) => {
+app.patch("/api/notes/:id", async (req, res) => {
   const id = req.params.id;
   const changedContent = req.body;
-  const note = await knex("notes").where({id: id}).update({title: changedContent.title, content: changedContent.content});
-  res.send("Yay");
-
-})
+  await knex("notes").where({ id: id }).update(changedContent);
+  res.sendStatus(204);
+});
 
 const startServer = async () => {
   console.log("Running migrations");
