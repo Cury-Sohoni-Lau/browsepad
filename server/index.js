364cc21fb743a64176c0aea1279790c1de80fedb
flpcury <26260549+flpcury@users.noreply.github.com>
2021-08-19 14:44:26 +0900

    Fix bug in endpoint

--- server/index.js
+++ server/index.js
@@ -101,7 +101,7 @@ app.get("/api/notes", checkAuth, async (req, res) => {
   res.send(result);
 });
 
-app.get("/api/notes/url", async (req, res) => {
+app.post("/api/notes/url", async (req, res) => {
   const result = await knex("notes").select().where({ url: req.body.url });
   res.send(result);
 });
