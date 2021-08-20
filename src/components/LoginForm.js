00768a947c8993b959f3ab92faa4bfd3026a25a1
mimzivvimzi <mdeniselau@gmail.com>
2021-08-20 14:10:06 +0900

    Add react-bootstrap to Login page and add Link to Registration Page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,8 +2,11 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 import { storeUserAndToken } from "../utils";
 import { Context } from "../Store";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
+import { Card, Button, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+ 
 export default function LoginForm() {
   const [, dispatch] = useContext(Context);
   const [email, setEmail] = useState("");
@@ -22,47 +25,31 @@ export default function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="email"
-          placeholder="adams@gmail.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    // <Form>
-    //   <Form.Group className="mb-3" controlId="formBasicEmail">
-    //     <Form.Label>Email</Form.Label>
-    //     <Form.Control type="email" placeholder="Enter email" />
-    //     <Form.Text className="text-muted">
-    //       We'll never share your email with anyone else.
-    //     </Form.Text>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3" controlId="formBasicPassword">
-    //     <Form.Label>Password</Form.Label>
-    //     <Form.Control type="password" placeholder="Password" />
-    //   </Form.Group>
-    //   <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    //     <Form.Check type="checkbox" label="Check me out" />
-    //   </Form.Group>
-    //   <Button variant="primary" type="submit">
-    //     Submit
-    //   </Button>
-    // </Form>
+    <Card style={{ width: '20rem', margin: '0 auto' }} className="test">
+      <Card.Body>
+        <Card.Title className="text-center">Login</Card.Title>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label className="mx-auto my-2">Email</Form.Label>
+            <Form.Control 
+              name="email"
+              type="email"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <Form.Label className="mx-auto my-2">Password</Form.Label>
+            <Form.Control 
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
+          </Form.Group>
+          <Button type="submit" className="mx-auto my-2">Submit</Button>
+          <p>New user?   
+            <Link to="/register" style={{ textDecoration: 'none' }}> Create an account</Link>
+          </p>
+        </Form>
+    </Card.Body>
+  </Card>
   );
 }
