1dc214b97512c4023e4a599855960904c65cc638
flpcury <26260549+flpcury@users.noreply.github.com>
2021-09-01 17:45:43 +0900

    feat: Improve login form
    Co-authored-by: Michelle <mdeniselau@gmail.com>
    Co-authored-by: rituraj8503 <rituraj8503@users.noreply.github.com>

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,8 +3,14 @@ import axios from "axios";
 import { storeUserAndToken, host } from "../utils";
 import { Context } from "../Store";
 import { useHistory, Link } from "react-router-dom";
-import { Card, Button, Form } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import { Card, Button, Form } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import TextField from '@material-ui/core/TextField'
+// import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+// import { teal } from "@material-ui/core/colors"
 
 export default function LoginForm() {
   const [, dispatch] = useContext(Context);
@@ -23,40 +29,84 @@ export default function LoginForm() {
     history.push("/notes");
   };
 
+  const useStyles = makeStyles({
+    root: {
+      background: "linear-gradient(45deg, #8860D0, #84CEEB)",
+      border: 0,
+      marginBottom: 15,
+      borderRadius: 100,
+      color: "white",
+      padding: "5px 30px",
+    },
+  });
+
+  function ButtonStyled() {
+    const classes = useStyles();
+    return (
+      <Button id="test" className={classes.root} onClick={handleSubmit}>
+        Login
+      </Button>
+    );
+  }
+
   return (
-    <Card style={{ width: "20rem", margin: "0 auto" }} className="test">
-      <Card.Body>
-        <Card.Title className="text-center">Login</Card.Title>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Label className="mx-auto my-2">Email</Form.Label>
-            <Form.Control
-              name="email"
-              type="email"
-              placeholder="example@gmail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Form.Label className="mx-auto my-2">Password</Form.Label>
-            <Form.Control
-              name="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button type="submit" className="mx-auto my-2">
-            Submit
-          </Button>
-          <p>
-            New user?
-            <Link to="/register" style={{ textDecoration: "none" }}>
-              {" "}
-              Create an account
-            </Link>
-          </p>
-        </Form>
-      </Card.Body>
-    </Card>
+    <div id="login-box">
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Email"
+          variant="filled"
+          type="email"
+          placeholder="user@example.com"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="filled"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <ButtonStyled onClick={handleSubmit} />
+      </form>
+      <p>
+        New user?
+        <Link to="/register" style={{ textDecoration: "none" }}>
+          {" "}
+          Create an account
+        </Link>
+      </p>
+    </div>
+
+    // <Card style={{ width: "20rem", margin: "0 auto" }} className="test">
+    //   <Card.Body>
+    //     <Card.Title className="text-center">Login</Card.Title>
+    //     <Form onSubmit={handleSubmit}>
+    //       <Form.Group>
+    //         <Form.Label className="mx-auto my-2">Email</Form.Label>
+    //         <Form.Control
+    //           name="email"
+    //           type="email"
+    //           placeholder="example@gmail.com"
+    //           value={email}
+    //           onChange={(e) => setEmail(e.target.value)}
+    //         />
+    //         <Form.Label className="mx-auto my-2">Password</Form.Label>
+    //         <Form.Control
+    //           name="password"
+    //           type="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </Form.Group>
+    //       <Button type="submit" className="mx-auto my-2">
+    //         Submit
+    //       </Button>
+
+    //     </Form>
+    //   </Card.Body>
+    // </Card>
   );
 }
